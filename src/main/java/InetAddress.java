8d6c72daf0541d7420bac151c5047e4755f504f5
rasool <rasools27@gmail.com>
2018-08-21 15:26:20 +0530

    add file

---
+++ src/main/java/InetAddress.java
@@ -0,0 +1,14 @@
+
+public class InetAddress {
+
+	public char[] getHostAddress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static InetAddress getLocalHost() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/getmyTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class getmyTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
